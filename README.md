d9391b796568c7a6bcf3c4b8cd37188a9a250d81
warsan <warsan00@mail.ru>
2021-01-09 18:52:49 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# gsap-react-simple-tween-p2damb
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/gsap-react-simple-tween-p2damb)
---
+++ simple-tween.js
@@ -0,0 +1,74 @@
+import React, { Component } from "react";
+import { TimelineLite, CSSPlugin } from "gsap/all";
+
+
+class SimpleTween extends Component {
+
+	constructor(props){
+		super(props);
+		// logo container
+		this.logoContainer = null;
+		// logo tween
+		this.logoTween = null;
+	}
+
+
+	componentDidMount(){
+		// create logo tween
+		this.logoTween = new TimelineLite({ paused:true })
+			.to(this.logoContainer, 2, { x: 500 })
+			.to(this.logoContainer, 1, { rotation: 360, transformOrigin: "center" });
+	}
+
+
+	render(){
+		return <div className="container">
+			<div className="row">
+
+				<div className="col-12 mt-3">
+					<h3 className="text-center">Простая Анимация</h3>
+					<p>Анимирует логотип GSAP справа от его исходного положения и, наконец, делает поворот на 360 градусов. Вы можете использовать кнопки для управления анимацией.</p>
+					<p>Использует встроенный обратный вызов <strong>ref</strong> для создания ссылки на элемент DOM, который затем используется в событии <strong>componentDidMount</strong> для создания экземпляра GSAP.</p>
+					<hr/>
+				</div>
+
+				<div className="col-12">
+					<h3 className="text-center">Управление логотипом Tween</h3>
+					<p>Используйте кнопки для управления Logo Tween</p>
+					<div className="mb-2 btn-group">
+						<button
+							className="btn gsap-btn"
+							onClick={() => this.logoTween.play()}
+						>Старт</button>
+						<button
+							className="btn gsap-btn"
+							onClick={() => this.logoTween.pause()}
+						>Пауза</button>
+						<button
+							className="btn gsap-btn"
+							onClick={() => this.logoTween.reverse()}
+						>Реверс</button>
+						<button
+							className="btn gsap-btn"
+							onClick={() => this.logoTween.restart()}
+						>Рестарт</button>
+					</div>
+					<hr />
+				</div>
+
+				<div className="col-12 mt-3">
+					<img
+						src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/16327/logo-man.svg"
+						alt=""
+						className="img-fluid logo"
+						ref={ img => this.logoContainer = img }
+					/>
+				</div>
+
+			</div>
+		</div>;
+	}
+	
+}
+
+export default SimpleTween;
